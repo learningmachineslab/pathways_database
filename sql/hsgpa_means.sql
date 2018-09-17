16f7027814b38bf969684d89e09ae3c6a9e0ba96
John Aiken <johnm.aiken@gmail.com>
2018-09-17 13:05:28 +0200

    now includes percentages

--- sql/hsgpa_means.sql
+++ sql/hsgpa_means.sql
@@ -2,6 +2,7 @@
 SELECT ROUND(AVG(hs_gpa), 3) AS [mean hs gpa]
 , ROUND(SQRT(STDEV(hs_gpa))/COUNT(hs_gpa), 3) as [95% confidence]
 , COUNT(hs_gpa) as cnt
+, COUNT(hs_gpa)*100.0/(SELECT COUNT(student_id_fk) FROM Students WHERE Students.first_course_datetime BETWEEN '1993' AND '2013') AS [Percent]
 , hs_GPA_type_code
 FROM Students AS s
 WHERE first_course_datetime BETWEEN '1993' AND '2013'
@@ -9,3 +10,8 @@ AND hs_gpa IS NOT NULL
 GROUP BY hs_GPA_type_code
 ORDER BY cnt DESC
 
+SELECT COUNT(*)
+,  COUNT(*)*100.0/(SELECT COUNT(*) FROM Students WHERE Students.first_course_datetime BETWEEN '1993' AND '2013') AS [Percent]
+FROM Students AS s
+WHERE first_course_datetime BETWEEN '1993' AND ' 2013'
+AND hs_gpa IS NULL
